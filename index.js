e9016eba8281ce4ca0252054b557d8ca1278e13d
Zayac11 <brunogrbstudios@gmail.com>
2020-09-28 23:38:09 +0300

    first

---
+++ index.js
@@ -0,0 +1,279 @@
+const animItems = document.querySelectorAll('.animItems');
+
+if(animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('active');
+            }  else {
+                    if(!animItem.classList.contains('anim-no-hide')) {
+                       animItem.classList.remove('active'); 
+                    }
+                }
+
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft
+        }
+    }
+    setTimeout( () => {
+        animOnScroll();
+    }, 300 )
+    
+
+}
+
+
+function Sim(sldrId) {
+
+	let id = document.getElementById(sldrId);
+	if(id) {
+		this.sldrRoot = id
+	}
+	else {
+		this.sldrRoot = document.querySelector('.sim-slider')
+	};
+
+	// Carousel objects
+	this.sldrList = this.sldrRoot.querySelector('.sim-slider-list');
+	this.sldrElements = this.sldrList.querySelectorAll('.sim-slider-element');
+	this.sldrElemFirst = this.sldrList.querySelector('.sim-slider-element');
+	this.leftArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-left');
+	this.rightArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-right');
+	this.indicatorDots = this.sldrRoot.querySelector('div.sim-slider-dots');
+
+	// Initialization
+	this.options = Sim.defaults;
+	Sim.initialize(this)
+};
+
+Sim.defaults = {
+
+	// Default options for the carousel
+	loop: true,     // Бесконечное зацикливание слайдера
+	auto: true,     // Автоматическое пролистывание
+	interval: 5000, // Интервал между пролистыванием элементов (мс)
+	arrows: true,   // Пролистывание стрелками
+	dots: true      // Индикаторные точки
+};
+
+Sim.prototype.elemPrev = function(num) {
+	num = num || 1;
+
+	let prevElement = this.currentElement;
+	this.currentElement -= num;
+	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
+
+	if(!this.options.loop) {
+		if(this.currentElement == 0) {
+			this.leftArrow.style.display = 'none'
+		};
+		this.rightArrow.style.display = 'block'
+	};
+	
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+	if(this.options.dots) {
+		this.dotOn(prevElement); this.dotOff(this.currentElement)
+	}
+};
+
+Sim.prototype.elemNext = function(num) {
+	num = num || 1;
+	
+	let prevElement = this.currentElement;
+	this.currentElement += num;
+	if(this.currentElement >= this.elemCount) this.currentElement = 0;
+
+	if(!this.options.loop) {
+		if(this.currentElement == this.elemCount-1) {
+			this.rightArrow.style.display = 'none'
+		};
+		this.leftArrow.style.display = 'block'
+	};
+
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+	if(this.options.dots) {
+		this.dotOn(prevElement); this.dotOff(this.currentElement)
+	}
+};
+
+Sim.prototype.dotOn = function(num) {
+	this.indicatorDotsAll[num].style.cssText = 'background-color:#BBB; cursor:pointer;'
+};
+
+Sim.prototype.dotOff = function(num) {
+	this.indicatorDotsAll[num].style.cssText = 'background-color:#556; cursor:default;'
+};
+
+Sim.initialize = function(that) {
+
+	// Constants
+	that.elemCount = that.sldrElements.length; // Количество элементов
+
+	// Variables
+	that.currentElement = 0;
+	let bgTime = getTime();
+
+	// Functions
+	function getTime() {
+		return new Date().getTime();
+	};
+	function setAutoScroll() {
+		that.autoScroll = setInterval(function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime + 10 > that.options.interval) {
+				bgTime = fnTime; that.elemNext()
+			}
+		}, that.options.interval)
+	};
+
+	// Start initialization
+	if(that.elemCount <= 1) {   // Отключить навигацию
+		that.options.auto = false; that.options.arrows = false; that.options.dots = false;
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+	if(that.elemCount >= 1) {   // показать первый элемент
+		that.sldrElemFirst.style.opacity = '1';
+	};
+
+	if(!that.options.loop) {
+		that.leftArrow.style.display = 'none';  // отключить левую стрелку
+		that.options.auto = false; // отключить автопркрутку
+	}
+	else if(that.options.auto) {   // инициализация автопрокруки
+		setAutoScroll();
+		// Остановка прокрутки при наведении мыши на элемент
+		that.sldrList.addEventListener('mouseenter', function() {clearInterval(that.autoScroll)}, false);
+		that.sldrList.addEventListener('mouseleave', setAutoScroll, false)
+	};
+
+	if(that.options.arrows) {  // инициализация стрелок
+		that.leftArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; that.elemPrev()
+			}
+		}, false);
+		that.rightArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; that.elemNext()
+			}
+		}, false)
+	}
+	else {
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+
+	if(that.options.dots) {  // инициализация индикаторных точек
+		let sum = '', diffNum;
+		for(let i=0; i<that.elemCount; i++) {
+			sum += '<span class="sim-dot"></span>'
+		};
+		that.indicatorDots.innerHTML = sum;
+		that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.sim-dot');
+		// Назначаем точкам обработчик события 'click'
+		for(let n=0; n<that.elemCount; n++) {
+			that.indicatorDotsAll[n].addEventListener('click', function() {
+				diffNum = Math.abs(n - that.currentElement);
+				if(n < that.currentElement) {
+					bgTime = getTime(); that.elemPrev(diffNum)
+				}
+				else if(n > that.currentElement) {
+					bgTime = getTime(); that.elemNext(diffNum)
+				}
+				// Если n == that.currentElement ничего не делаем
+			}, false)
+		};
+		that.dotOff(0);  // точка[0] выключена, остальные включены
+		for(let i=1; i<that.elemCount; i++) {
+			that.dotOn(i)
+		}
+	}
+};
+
+new Sim();
+
+
+
+// $(document).ready(function () {
+//     const navOffset = $('#header').offset().top;
+//     $(window).scroll(function () {
+//         const scrolled = $(this).scrollTop();
+
+//         if(scrolled > navOffset) {
+//             $('#header').addClass("header_fixed");
+//         } else if (scrolled < navOffset) {
+//             $('#header').removeClass("header_fixed");
+//         }
+//     })
+// });
+
+
+
+
+
+// window.onscroll = function showHeader() {
+//     var header = document.querySelector('.header');
+
+//     if(window.pageYOffset > 100) {
+//         header.classList.add('header_fixed');
+//     } else {
+//         header.classList.remove('header_fixed');
+//     }
+// }
+
+// window.onscroll = function paddingContent() {
+//     var header = document.querySelector('.container');
+
+//     if(window.pageYOffset > 100) {
+//         header.classList.add('container_padding');
+//     } else {
+//         header.classList.remove('container_padding');
+//     }
+// }
+
+// $('#navigation').find('li').find('a').each(function() {
+//     if(($(this).attr('href')==$(location).attr('href')) 
+//     || ($(this).attr('href')==$(location).attr('pathname'))){
+//            $(this).parent.addClass("nav_item_active");
+//     }
+// });
+
+// $("a[href*='" + location.pathname + "']").addClass("nav_item_active ");
+
+// var nav = document.querySelectorAll('.nav_item');
+
+// window.addEventListener('click', setActiveClass);
+
+// function setActiveClass() {
+//     for (let i = 0; i < nav.length; i++) {
+//         const navItem = nav[i];
+//         if(navItem.classList.contains('nav_item_active')) {
+//             navItem.classList.remove('nav_item_active'); 
+//         }
+//         else navItem.classList.add('nav_item_active')
+//     }
+// }
+
+// onclick="this.className = (this.className == 'nav_item' ? 'nav_item_active' : 'nav_item')"
